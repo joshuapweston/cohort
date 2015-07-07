410d21f9ae19196835b3dc167e428202e1dfb0cb
joshuapweston <jpweston82@gmail.com>
2015-07-06 17:05:32 -0700

    try2

---
+++ desktop/cohort/app.js
@@ -0,0 +1,20 @@
+/**
+Prompt for a number value (provide a default of 10)
+Prompt for another number value (provide a default of 10)
+Convert the promped values into integers.
+Make sure that the input from steps 1 and 2 are numbers.
+If they are numbers, add the numbers together and log the value to the console.
+If either of the prompted values are not numbers, alert someting to the user that they must input nubmers only.
+*/
+
+var num1 = prompt("please input a number.", 10),
+	int1 = parseInt(num1),
+    num2 = prompt("please input another number", 10),
+	int2 = parseInt(num2);
+
+if(int1 || int2 == isNaN){
+	console.log('thanks for paying attention')
+} else {
+	
+	console.log('I need numbers not strings!')
+}
